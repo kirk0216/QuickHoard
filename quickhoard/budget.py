ef79c42d0054d9641dd2ca236157d9106d1d21ae
Patrick Kirk <kirk0216@algonquinlive.com>
2021-11-14 22:28:03 -0500

    Dashboard now provides a summary of income, expenses, and categories.

--- quickhoard/budget.py
+++ quickhoard/budget.py
@@ -6,7 +6,31 @@ bp = Blueprint('budget', __name__)
 
 @bp.route('/')
 def index():
-    if session.get('user_id') is None:
+    user_id = session.get('user_id')
+
+    if user_id is None:
         return redirect(url_for('auth.login'))
 
-    return render_template('dashboard.html')
+    database = Database()
+    database.open()
+
+    sql = (
+        'SELECT b.Id, b.User_Id, b.Name, c.Name AS Category, c.Goal, '
+        '	COALESCE(ABS(SUM(t.Amount)), 0) AS Spent, '
+        '    COALESCE(c.Goal - ABS(SUM(t.Amount)), c.Goal) AS Remaining, '
+        '	(SELECT COALESCE(ABS(SUM(Amount)), 0) FROM `transaction` WHERE Amount < 0) AS Expense, '
+        '    (SELECT COALESCE(ABS(SUM(Amount)), 0) FROM `transaction` WHERE Amount > 0) AS Income '
+        'FROM budget b '
+        'LEFT JOIN category c ON c.Budget_Id = b.Id '
+        'LEFT JOIN `transaction` t ON t.Category_Id = c.Id '
+        'WHERE b.User_Id = %s '
+        'GROUP BY c.Id;'
+    )
+
+    cursor = database.query(sql, user_id)
+    budget = cursor.fetchall()
+    print(budget)
+
+    database.close()
+
+    return render_template('dashboard.html', budget=budget)
