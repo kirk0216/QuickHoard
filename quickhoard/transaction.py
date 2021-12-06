0c661758762862f66745b1617a43de9a5ab40edd
Patrick Kirk <kirk0216@algonquinlive.com>
2021-12-05 20:01:35 -0500

    Moved Transaction class to its own file.

--- quickhoard/transaction.py
+++ quickhoard/transaction.py
@@ -2,37 +2,9 @@ from flask import Blueprint, render_template, session, redirect, url_for, reques
 from quickhoard.db import Database
 from quickhoard.budget import Category
 from datetime import date
+from quickhoard.model.transaction import Transaction
 
 bp = Blueprint('transaction', __name__, url_prefix='/transaction')
-
-
-class Transaction:
-    def __init__(self, date=None, recipient=None, category_id=None, amount=None, category=None):
-        self.id = None
-        self.date = date
-        self.recipient = recipient
-        self.category_id = category_id
-        self.category = category
-        self.amount = amount
-
-    def parse(self, row):
-        if 'id' in row:
-            self.id = row['id']
-
-        if 'date' in row:
-            self.date = row['date']
-
-        if 'recipient' in row:
-            self.recipient = row['recipient']
-
-        if 'category_id' in row:
-            self.category_id = row['category_id']
-
-        if 'category' in row:
-            self.category = row['category']
-
-        if 'amount' in row:
-            self.amount = row['amount']
 
 
 @bp.route('/')
